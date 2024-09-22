c1045e31757745ead5644101c71fe35673b5b227
pminhtri <minhtri19042001@gmail.com>
2024-09-22 15:08:24 +0700

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: 'src/index.ts',
-      name: 'core-lib',
-      fileName: (format) => `core-lib.${format}.js`,
+      entry: path.resolve(__dirname, 'src/index.ts'), // Entry point of your library
+      name: 'core-lib', // Name of your library
+      fileName: (format) => `core-lib.${format}.js`, // Output file name
     },
     rollupOptions: {
-      external: ['react'],
+      // Ensure to externalize dependencies that shouldn't be bundled into your library
+      external: ['react', 'react-dom'],
       output: {
+        // Provide global variables to use in the UMD build for externalized deps
         globals: {
           react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
-  }
-})
+  },
+});
